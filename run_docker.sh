64121aeeb7d829c25247684f7d331f8d1cce5b24
Hodaya Gamliel <hodaya.gamliel@takipi.com>
2016-06-02 15:32:12 +0300

    hod

--- run_docker.sh
+++ run_docker.sh
@@ -6,7 +6,7 @@ IMAGE_NAME="hodaya/run1"
 SHARE_DIR=/home/ubuntu/docker-tests2/share
 
 #LOG_PATH=/vagrant/share
-docker build --build-arg JAR_FILE="test7"  URL="https://backend.takipi.com" SK="S10168#g3vpTbodqeT6uJcU#m+gGA6/5Pha8ycZoEEykoYfDzEK1XkV/4/tq8T4mnL0=#11c6"  -t $IMAGE_NAME .
+docker build --build-arg JAR_FILE="test7" --build-arg URL="https://backend.takipi.com" --build-arg SK="S10168#g3vpTbodqeT6uJcU#m+gGA6/5Pha8ycZoEEykoYfDzEK1XkV/4/tq8T4mnL0=#11c6"  -t $IMAGE_NAME .
 
 # docker build -t $IMAGE_NAME .
 
