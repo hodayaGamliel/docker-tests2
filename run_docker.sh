d6229e1c8004ebb8c8673fa81fb23365c0a13a93
Hodaya Gamliel <hodaya.gamliel@takipi.com>
2016-06-02 15:29:59 +0300

    hod

--- run_docker.sh
+++ run_docker.sh
@@ -2,12 +2,12 @@
 
 set -x
 
-IMAGE_NAME="hodaya/run"
+IMAGE_NAME="hodaya/run1"
 SHARE_DIR=/home/ubuntu/docker-tests2/share
 
 #LOG_PATH=/vagrant/share
-#docker build --build-arg JAR_FILE="scala-boom1.jar"  URL="https://backend.takipi.com" SK="S12974#v+OrCVNCP5zNbq0G#wY00LeBhPNV6U1wJY94aiZHdg08YvXNO1oDlettWeS4=#42e3"  -t $IMAGE_NAME .
+docker build --build-arg JAR_FILE="test7"  URL="https://backend.takipi.com" SK="S10168#g3vpTbodqeT6uJcU#m+gGA6/5Pha8ycZoEEykoYfDzEK1XkV/4/tq8T4mnL0=#11c6"  -t $IMAGE_NAME .
 
-docker build -t $IMAGE_NAME .
+# docker build -t $IMAGE_NAME .
 
 docker run -v $SHARE_DIR:/tmp/share/ $IMAGE_NAME
