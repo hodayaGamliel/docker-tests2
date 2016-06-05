1c4ddb39e79a6f1070eab5208a289f48633b6822
Hodaya Gamliel <hodaya.gamliel@takipi.com>
2016-06-05 17:34:59 +0300

    hod

--- run_docker.sh
+++ run_docker.sh
@@ -5,9 +5,13 @@ set -x
 IMAGE_NAME="hodaya/run1"
 SHARE_DIR=/home/ubuntu/docker-tests2/share
 
-C_JAR_FILE="test7"
-C_URL="https://backend.takipi.com"
-C_SK="S10168#g3vpTbodqeT6uJcU#m+gGA6/5Pha8ycZoEEykoYfDzEK1XkV/4/tq8T4mnL0=#11c6"
+C_JAR_FILE=$1
+C_URL=$2
+C_SK=$3
+
+# C_JAR_FILE="test7"
+# C_URL="https://backend.takipi.com"
+# C_SK="S10168#g3vpTbodqeT6uJcU#m+gGA6/5Pha8ycZoEEykoYfDzEK1XkV/4/tq8T4mnL0=#11c6"
 
 docker build --build-arg JAR_FILE=$C_JAR_FILE --build-arg URL=$C_URL --build-arg SK=$C_SK  -t $IMAGE_NAME .
 
